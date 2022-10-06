6fb5265d34638ba04a31d8dce4184e449e3a1706
Vlad-Luts1uk <113033909+Vlad-Luts1uk@users.noreply.github.com>
2022-10-06 09:51:07 -0400

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,19 @@
+age = int(input("whatis your age?"))
+
+if age >= 17:
+  print ("You can watch a R rated movie alone")
+
+elif age >= 13:
+  print ("You can watch PG movie alone")
+
+elif age >= 5:
+  print ("You can watch G or PG movies alone")
+
+elif age >= 3:
+  print ("Ask your parents to watch the movie with you")
+
+elif age >= 2:
+  print ("Smart childe")
+
+elif age >= 1:
+  print ("Just How")
